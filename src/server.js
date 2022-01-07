b5e91d0c3eeb7c09d4c6f10ce3bec18b3fe0c529
unknown <battikh.karama@gmail.com>
2022-01-07 11:18:54 +0100

    first commit init project

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import { ApolloServer, gql } from 'apollo-server';
+import books from './mockData';
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
